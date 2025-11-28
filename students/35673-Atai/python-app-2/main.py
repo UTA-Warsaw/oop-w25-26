fae4daad2e954fb76308e421e7ed950363f5f040
Atai <95021081+Atai225@users.noreply.github.com>
2025-11-28 14:59:22 +0000

    Printed variables

--- students/35673-Atai/python-app-2/main.py
+++ students/35673-Atai/python-app-2/main.py
@@ -1,4 +1,5 @@
 #Practising python basics (string, number)
+
 x = 9
 y = 2
 z = 3
@@ -7,4 +8,5 @@ string_1="Everything"
 string_2="is"
 string_3="possible"
 
-print(x, y, z, string_1, string_2, string_3)
+print(x, y, z)
+print(string_1, string_2, string_3)
