1d57400512e826ca42f5b91bb9fe20d68d9369b5
Estel35682 <sadykovaestel@gmail.com>
2026-01-23 13:20:56 +0000

    My changes

--- students/35682 Estel/main.py
+++ students/35682 Estel/main.py
@@ -1,1 +1,12 @@
+class Cat:
+    total_count = 0
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        type(self).total_count += 1
+
+c1 = Cat("Whiskers", "black")
+c2 = Cat("Yasna", "white")
+c3 = Cat("Happy", "orange")
+
 
